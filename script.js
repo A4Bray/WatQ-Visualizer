5f7dca6fa86252f1794bfc2f244db74ea1fca181
Andrew Bray (Nano 2A) <a4bray@uwaterloo.ca>
2026-09-08 14:59:02 -0500

    Fix typo in basis labels array

--- script.js
+++ script.js
@@ -2322,7 +2322,7 @@ function updateTwoQubitDisplay(shouldAnimate = true) {
                 "(" +
                 formatComplex(amplitude) +
                 ")|" +
-                TwqQubitBasisLabels[index] +
+                TwoQubitBasisLabels[index] +
                 "⟩"
             );
         }
